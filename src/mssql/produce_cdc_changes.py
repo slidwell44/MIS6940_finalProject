8f93786d5f62b902607ac60aa3e404706faa97b5
slidwell44 <slidwell@gmail.com>
2024-11-30 21:43:43 -0500

    streaming workorder changes from cdc into kafka

--- src/mssql/produce_cdc_changes.py
+++ src/mssql/produce_cdc_changes.py
@@ -2,8 +2,8 @@ import json
 import logging
 import time
 
-from sqlalchemy import text, LABEL_STYLE_NONE
-from sqlalchemy.engine.cursor import ResultFetchStrategy
+from sqlalchemy import text
+from sqlalchemy.exc import ResourceClosedError
 
 from src.db import get_connection, WiDatabases
 from src.wikafka import McpKafkaTopics, WiKafka
@@ -20,8 +20,7 @@ def convert_bytes_to_strings(records):
         new_record = {}
         for key, value in record.items():
             if isinstance(value, bytes):
-                new_record[key] = value.hex()  # Convert bytes to hex string
-                # Alternatively, use value.decode('utf-8', errors='replace') if applicable
+                new_record[key] = value.hex()
             else:
                 new_record[key] = value
         converted.append(new_record)
@@ -42,17 +41,25 @@ class WorkOrderChangesProducer:
     def fetch_cdc_changes(self):
         conn = get_connection(WiDatabases.LOCALDB)
         with conn.session_scope() as session:
-            lsn = session.execute()
             result = session.execute(
                 text(
                     """EXEC dbo.Get_CDC_Changes"""
                 )
             )
-            columns = result.keys()
-            rows = result.fetchall()
+
+            if not result.returns_rows:
+                logger.info("No CDC changes found (no result set returned).")
+                return
+
+            try:
+                columns = result.keys()
+                rows = result.fetchall()
+            except ResourceClosedError:
+                logger.info("No CDC changes found (result set is closed).")
+                return
 
         if not rows:
-            logger.info("No CDC changes found.")
+            logger.info("No CDC changes found (empty result set).")
             return
 
         db_changes = [dict(zip(columns, row)) for row in rows]
@@ -79,6 +86,6 @@ if __name__ == "__main__":
     try:
         while True:
             changes.fetch_cdc_changes()
-            time.sleep(5)
+            time.sleep(10)
     except KeyboardInterrupt:
         logger.debug("Exiting...")
